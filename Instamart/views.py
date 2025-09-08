16d9d27629b79939a8d56fc4e7ceaf86558f520d
Phani Raveendra Ravuri <raveendraravuri7@outlook.com>
2025-09-08 07:24:08 +0530

    Delete Instamart directory

--- Instamart/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def Chips(request):
-    return HttpResponse("<h2>Lays is always Best!</h2>")
-def DairyMilk(request):
-    return HttpResponse("<h1>Always Worst!!</h1>")
